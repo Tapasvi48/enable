3a0251edc916a0b5deb78db9a9a7de9ca69f1b6d
Tapasvi48 <Tapasviarora2002@gmail.com>
2023-11-21 15:19:13 +0530

    done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div style= {{backgroundImage : "linear-gradient(#00d5ff,#0095ff,rgba(93,0,255,.555))"}} className="d-flex flex-column justify-content-center align-items-center text-center vh-100">
         <h1>Login Success Page</h1>
-        <Link to='/login' className="btn btn-light my-5">Logout</Link>
+        <Link to='/login' className="btn btn-light my-5">Login</Link>
     </div>
   )
 }

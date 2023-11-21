364824c17321c732e43c45463058aa535324aaf9
Tapasvi48 <Tapasviarora2002@gmail.com>
2023-11-21 19:12:56 +0530

    done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,7 @@
 // import React from 'react';
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
   const button=async()=>{

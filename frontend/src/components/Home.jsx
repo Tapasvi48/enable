98e844dc3ccd9f92a0ff4fa82fbd6a744475cfcf
Tapasvi48 <Tapasviarora2002@gmail.com>
2023-11-21 20:16:35 +0530

    done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,19 +4,14 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Home = () => {
-  const button=async()=>{
-    const result= await axios.get( 'https://enable-nine.vercel.app/');
-    console.log(result);
 
-
-  }
   
 
 
   return (
     <div style= {{backgroundImage : "linear-gradient(#00d5ff,#0095ff,rgba(93,0,255,.555))"}} className="d-flex flex-column justify-content-center align-items-center text-center vh-100">
         <h1>Login Success Page</h1>
-<button onClick={button}><h1>BUTTOL</h1></button>
+
         <Link to='/login' className="btn btn-light my-5">Login</Link>
     </div>
   )

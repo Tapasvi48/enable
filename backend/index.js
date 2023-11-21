cb2ca8cea6094e7d160066ba6c91d7db06886d72
Tapasvi48 <Tapasviarora2002@gmail.com>
2023-11-21 19:30:44 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
-app.use(cors({
-    origin: 'https://enable-nxc4.vercel.app',
-    credentials:true,
-    methods:["GET","POST","PUT","DELETE"]
-  }));
+// app.use(cors());
+// app.use(cors({
+//     origin: 'https://enable-nxc4.vercel.app',
+//     credentials:true,
+//     methods:["GET","POST","PUT","DELETE"]
+//   }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/practice_mern');
 

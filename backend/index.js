3ef93695d2c2f6f0b3d2ea528216252353cf1a4b
Tapasvi48 <Tapasviarora2002@gmail.com>
2023-11-21 19:26:37 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -24,12 +24,14 @@ app.use(cors({
 
 mongoose.connect('mongodb://127.0.0.1:27017/practice_mern');
 
+app.use("/",(req,res)=>{
+res.json({message:"Hello "});
 
+
+})
 app.use("/api/v1", User);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+
 
 
 
